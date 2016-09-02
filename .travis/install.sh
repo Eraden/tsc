5ab580f0348c9548b6f3dc94d87e4e743e790ead
Eraden <adrian.wozniak.1986@gmail.com>
2016-09-02 15:12:39 +0200

    Fix CI

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,16 +3,16 @@
 mkdir -p build
 mkdir -p tmp
 
-if [ "$(cmake --version)" -eq "3.6.1" ]
+if [ "$(cmake --version | grep -E '[0-9]+.[0-9]+.[0-9]+' | sed 's/[a-z ]//gi')" -eq "3.6.1" ]
 then
   echo "Valid cmake"
 else
   cd tmp
   wget https://cmake.org/files/v3.6/cmake-3.6.1.tar.gz
-  tar -xvf cmake-3.6.1.tar.gz
+  tar -xvf cmake-3.6.1.tar.gz &> /dev/null
   cd cmake-3.6.1
-  ./configure
-  make -j 4
+  ./configure &> /dev/null
+  make -j 4 &> /dev/null
   sudo make install
 fi
 
