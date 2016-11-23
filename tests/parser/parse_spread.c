7161420220e1c5bd635e5c20fb348e0b16ed7e5f
Eraden <adrian.wozniak@ita-prog.pl>
2016-11-23 16:43:33 +0100

    rm cmake-build-debug

--- tests/parser/parse_spread.c
+++ tests/parser/parse_spread.c
@@ -1,6 +1,34 @@
 #include "./parse_spread.h"
 
 START_TEST(parse_valid_spread)
+  TSFile *tsFile = TS_parse_file("./examples/spread/input/valid.ts");
+
+  ck_assert_ptr_ne(tsFile, NULL);
+  ck_assert_tsFile_valid(tsFile);
+  ck_assert_ptr_ne(tsFile->tokens, NULL);
+  ck_assert_uint_eq(tsFile->tokensSize, 4);
+
+  TSParserToken **tokens = tsFile->tokens;
+  TSParserToken *args = NULL, *arg = NULL, *spread = NULL;
+  TSParserToken *fn = NULL, *classToken = NULL, *mth = NULL;
+
+  /* function first(...args) {} */
+  fn = tokens[0];
+  ck_validate_child(fn, TS_FUNCTION, L"first", 3);
+
+  tokens += 1;
+  /* function second(firstArgument, ...functionRest) {} */
+  fn = tokens[0];
+
+  tokens += 1;
+  /* class Example { constructor(...constructorArgs) {} method(methodArgument, ...methodRest) {} } */
+  classToken = tokens[0];
+
+  tokens += 1;
+  /* class Child extends Example { secondMethod(...secondMethodArgs) { this.method(1, ...secondMethodArgs); } } */
+  classToken = tokens[0];
+
+  TS_free_ts_file(tsFile);
 END_TEST
 
 void parse_spread_suite(Suite *suite) {
--- tests/support/children_validation.h
+++ tests/support/children_validation.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <cts/register.h>
+#include "./ck_token_type.h"
 #include <check.h>
 
 #define _ck_validate_child(child, type, name, size) \
 { \
   ck_assert_ptr_ne(child, NULL); \
-  ck_assert_tsToken_eq(child->tokenType, type); \
+  ck_assert_tsType_eq(child->tokenType, type); \
   if (name) { \
     ck_assert_ptr_ne(child->name, NULL); \
-    ck_assert_wstr_eq(child->name, name); \
+    ck_assert_wstr_eq((const wchar_t *) child->name, name); \
   } else ck_assert_ptr_eq(child->name, NULL); \
   ck_assert_uint_eq(child->childrenSize, size); \
   if (size) { \
@@ -17,4 +18,4 @@
   } else ck_assert_ptr_eq(child->children, NULL); \
 }
 
-#define ck_validate_child(child, type, name, size) _ck_validate_child(child, type, (const wchar_t *) name, (unsigned int) size)
+#define ck_validate_child(child, type, name, size) _ck_validate_child(child, type, name, size)
